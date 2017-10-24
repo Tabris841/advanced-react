812a6c462c97042dc25417a748f1e96ac85ab6aa
Anton Todorov <shadowtabris84@gmail.com>
2017-10-24 23:23:29 +0300

    setting up react

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './lib/components/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_module/,
+      use: 'babel-loader'
+    }]
+  }
+};
+
+module.exports = config;
