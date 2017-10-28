37a12028bf71f6aaef9372deb0ec9aea254a6d65
Anton Todorov <shadowtabris84@gmail.com>
2017-10-29 02:21:11 +0300

    workin on react optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 const config = {
-  entry: ['babel-polyfill', './lib/components/index.js'],
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules')
+    ]
   },
   module: {
     rules: [{
